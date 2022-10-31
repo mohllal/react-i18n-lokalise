630e7a9658602f775ed54b6024b5f45c3debb7cc
Kareem Khaled <kareem.mohllal@gmail.com>
2022-10-31 18:43:33 +0200

    Use branch name in uploading file to Lokalise

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,9 +2,17 @@ set -ex
 
 curl -sfL https://raw.githubusercontent.com/lokalise/lokalise-cli-2-go/master/install.sh | sh
 
+# create branch
+./bin/lokalise2 \
+    branch create \
+    --name=$BRANCH_NAME \
+    --token=$LOKALISE_TOKEN \
+    --project-id=$LOKALISE_PROJECT_ID \
+    || true
+
 ./bin/lokalise2 \
     file upload \
-    --file $3 \
-    --lang-iso $4 \
-    --token $1 \
-    --project-id $2
+    --file=$TRANSLATION_FILE \
+    --lang-iso=$LANGUAGE_ISO \
+    --token=$LOKALISE_TOKEN \
+    --project-id=$LOKALISE_PROJECT_ID:$BRANCH_NAME
