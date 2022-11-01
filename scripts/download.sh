2a35c6d97d48d3aec2d4321b2afe2384bb48ccc8
Kareem Khaled <kareem.mohllal@gmail.com>
2022-11-01 13:38:30 +0200

    Use branch name in downloading file from Lokalise

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,8 +3,8 @@ set -ex
 curl -sfL https://raw.githubusercontent.com/lokalise/lokalise-cli-2-go/master/install.sh | sh
 
 ./bin/lokalise2 \
-    --token $1 \
-    --project-id $2 \
+    --token=$LOKALISE_TOKEN \
+    --project-id=$LOKALISE_PROJECT_ID:$BRANCH_NAME \
     file download \
     --format json \
-    --unzip-to $3
+    --unzip-to=$TRANSLATION_FILE
